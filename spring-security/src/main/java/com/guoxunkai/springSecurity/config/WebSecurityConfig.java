b219fd44654290aa200d8404b8663e945f2a4629
guoxuankai <xkguo104@fiberhome.com>
2018-12-08 18:05:32 +0800

    增加记住我功能

--- spring-security/src/main/java/com/guoxunkai/springSecurity/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/guoxunkai/springSecurity/config/WebSecurityConfig.java
@@ -2,10 +2,17 @@ package com.guoxunkai.springSecurity.config;
 
 import com.guoxunkai.springSecurity.filter.ValidateCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.jnlp.PersistenceService;
+import javax.sql.DataSource;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,14 +26,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ValidateCodeFilter validateCodeFilter;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class).formLogin().loginPage("/login")
                 .loginProcessingUrl("/security/login")
                 .successHandler(myAuthenticationSuccessHandler)
                 .failureHandler(myAuthenticationFailureHandler)
+                .and().rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(2000)
+                .userDetailsService(userDetailsService)
                 .and().authorizeRequests()
-                .antMatchers("/login","/imgCode")
+                .antMatchers("/login","/imgCode","/hello")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
